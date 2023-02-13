d7c66dcc01cb9cd167de9470ef79c655904dca39
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-02-13 15:45:07 +0200

    feat: :sparkles: add higher order functions

---
+++ week-3/HOF/DavidTheHoff.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+console.log(arr.filter((x) => x > 4))
+console.log(arr.filte((x) => x % 2 === 0))// even// === is type equivalence
+console.log(arr.findIndex((x) => x ===8));
+
